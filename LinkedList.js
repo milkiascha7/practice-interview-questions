dddc63a8f9ca90f0aeaa3b54f01eb61ec1e87982
milkiascha7 <mikehailu504@gmail.com>
2023-08-10 01:07:18 +0300

    "insert value with index in Linkedlist"

--- LinkedList.js
+++ LinkedList.js
@@ -29,13 +29,9 @@ class LinkedList {
     this.length = 1;
   }
 
+  // adding a node at the last
   append(value) {
     //   create the new node first with the same properties
-    // const newNode = {
-    //   value: value,
-    //   next: null,
-    // };
-
     const newNode = new Node(value);
 
     this.tail.next = newNode;
@@ -46,13 +42,7 @@ class LinkedList {
   }
 
   prepend(value) {
-    // const firstNode = {
-    //   value: value,
-    //   next: null,
-    // };
     const newNode = new Node(value);
-
-    // this.head.next = firstNode;
 
     newNode.next = this.head;
     this.head = newNode;
@@ -71,10 +61,46 @@ class LinkedList {
     }
     return array;
   }
+
+  insert(index, value) {
+    console.log('this', this.length);
+
+    // if index is larger than the entire list length add it to the end
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    const leader = this.traverseToIndex(index - 1);
+    const holdNextPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdNextPointer;
+
+    this.length++;
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.prepend(14);
 myLinkedList.printList();
+console.log(myLinkedList.printList());
+myLinkedList.insert(2, 28);
+myLinkedList.insert(200, 128);
+myLinkedList.insert(3, 13);
 console.log(myLinkedList.printList());
