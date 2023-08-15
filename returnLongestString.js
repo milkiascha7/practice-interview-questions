7dfab68ae9cb9fac81cfbea6f99983c51a9b32fe
milkiascha7 <mikehailu504@gmail.com>
2023-08-15 16:34:06 +0300

    "return longest string"

---
+++ returnLongestString.js
@@ -0,0 +1,16 @@
+function findLongestWordLength(str) {
+  let arrayWords = str.split(' ');
+  console.log('arrayWords', arrayWords[1]);
+
+  let maxLength = 0;
+  for (let i = 0; i < arrayWords.length; i++) {
+    if (arrayWords[i].length > maxLength) {
+      maxLength = arrayWords[i].length;
+      console.log(maxLength);
+    }
+  }
+  console.log('final answer', maxLength);
+  //   return arrayWords.length;
+}
+
+findLongestWordLength('The quick brown fox jumped over the lazy dog');
