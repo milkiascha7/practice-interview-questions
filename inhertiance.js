8712ab4c87ea81306b6b6290950868c94a10256f
milkiascha7 <mikehailu504@gmail.com>
2023-08-31 18:12:31 +0300

    "Reset an Inherited Constructor Property"

--- inhertiance.js
+++ inhertiance.js
@@ -47,6 +47,42 @@ function Dog() {}
 
 Dog.prototype = Object.create(Animal.prototype);
 
-let beagle = new Dog();
+let beagleOne = new Dog();
 
 // beagle inherits all of Animal's properties, including the eat method.
+
+// ----------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------
+
+// Reset an Inherited Constructor Property
+
+// When an object inherits its prototype from another object, it also inherits the supertype's constructor property.
+
+function Bird() {}
+Bird.prototype = Object.create(Animal.prototype);
+let duckOne = new Bird();
+duck.constructor;
+
+// But duck and all instances of Bird should show that they were constructed by Bird and not Animal.
+// To do so, you can manually set the constructor property of Bird to the Bird object:
+
+Bird.prototype.constructor = Bird;
+duck.constructor;
+
+function Animal() {}
+function Bird() {}
+function Dog() {}
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+
+// so to show that all the instances were constructed from Bird and Dog and not animal we do this
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+let duck = new Bird();
+let beagle = new Dog();
+duck.constructor;
+beagle.constructor;
