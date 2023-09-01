ba4fdbbf589e873a92bfcf590e022d7d0a239063
milkiascha7 <mikehailu504@gmail.com>
2023-09-01 04:17:22 +0300

    "Add Methods After Inheritance"

--- inhertiance.js
+++ inhertiance.js
@@ -82,7 +82,56 @@ Dog.prototype = Object.create(Animal.prototype);
 
 Bird.prototype.constructor = Bird;
 Dog.prototype.constructor = Dog;
+
 let duck = new Bird();
 let beagle = new Dog();
+
 duck.constructor;
 beagle.constructor;
+
+// --------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------
+
+// Add Methods After Inheritance
+
+// For example, Bird is a constructor that inherits its prototype from Animal:
+
+function Animal() {}
+Animal.prototype.eat = function () {
+  console.log('nom nom nom');
+};
+function Bird() {}
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.constructor = Bird;
+
+// In addition to what is inherited from Animal, you want to add behavior that is unique to Bird objects.
+// Here, Bird will get a fly() function. Functions are added to Bird's prototype the same way as any constructor function:
+
+Bird.prototype.fly = function () {
+  console.log("I'm flying!");
+};
+
+let duckTwo = new Bird();
+duckTwo.eat();
+duckTwo.fly();
+
+// Add all necessary code so the Dog object inherits from Animal and the Dog's prototype constructor is set to Dog.
+//  Then add a bark() method to the Dog object so that beagle can both eat() and bark().
+// The bark() method should print Woof! to the console.
+
+function AnimalTwo() {}
+AnimalTwo.prototype.eat = function () {
+  console.log('nom nom nom');
+};
+
+function DogTwo() {}
+
+// Only change code below this line
+
+DogTwo.prototype = Object.create(AnimalTwo.prototype); //inherit from the animal function
+DogTwo.prototype.constructor = DogTwo; // set the constructor to the dog function and not the supertype animal function
+DogTwo.prototype.bark = function () {
+  console.log('Woof!');
+};
+
+let beagleTwo = new Dog();
