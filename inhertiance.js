59e5baae9ed6e9637f7e6b5c5edf2926945025c1
milkiascha7 <mikehailu504@gmail.com>
2023-09-01 04:57:26 +0300

    "introduction to mixin"

--- inhertiance.js
+++ inhertiance.js
@@ -135,3 +135,38 @@ DogTwo.prototype.bark = function () {
 };
 
 let beagleTwo = new Dog();
+
+// --------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------
+
+// Use a Mixin to Add Common Behavior Between Unrelated Objects
+
+// For unrelated objects, it's better to use mixins. A mixin allows other objects to use a collection of functions.
+
+let flyMixin = function (obj) {
+  obj.fly = function () {
+    console.log('Flying, wooosh!');
+  };
+};
+
+// The flyMixin takes any object and gives it the fly method.
+
+let bird = {
+  name: 'Donald',
+  numLegs: 2,
+};
+
+let plane = {
+  model: '777',
+  numPassengers: 524,
+};
+
+flyMixin(bird);
+flyMixin(plane);
+
+// bird and plane are passed into flyMixin, which then assigns the fly function to each object.
+// Now bird and plane can both fly:
+bird.fly();
+plane.fly(); //The console would display the string Flying, wooosh! twice, once for each .fly() call.
+
+// the mixin allows for the same fly method to be reused by unrelated objects bird and plane.
