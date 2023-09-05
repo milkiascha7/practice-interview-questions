48064315472139b7b2ee4aeb22f31aa2180550de
milkiascha7 <mikehailu504@gmail.com>
2023-09-05 23:47:52 +0300

    "Implement map on a Prototype"

--- FunctionalProgramming.js
+++ FunctionalProgramming.js
@@ -45,3 +45,37 @@ const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
 // Only change code above this line
 
 console.log(tea4GreenTeamFCC, tea4BlackTeamFCC);
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------------
+
+// Implement map on a Prototype
+
+// Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map().
+// You should not use the built-in map method. The Array instance can be accessed in the myMap method using this.
+
+// question
+Array.prototype.myMap = function (callback) {
+  const newArray = [];
+  // Only change code below this line
+
+  // Only change code above this line
+  return newArray;
+};
+
+// solution
+Array.prototype.myMap = function (callback) {
+  const newArray = [];
+  // Only change code below this line
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i], i, this));
+  }
+
+  // OR
+  this.forEach((element, index, originalArr) =>
+    newArray.push(callback(element, index, originalArr))
+  );
+
+  // Only change code above this line
+  return newArray;
+};
