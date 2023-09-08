ba05dea60f3d95c4fa758efe5aaec1ccc388fb24
milkiascha7 <mikehailu504@gmail.com>
2023-09-08 19:37:33 +0300

    "Use Higher-Order Functions map, filter, or reduce"

--- reduceMethod.js
+++ reduceMethod.js
@@ -199,3 +199,35 @@ function getRating(watchList) {
 }
 
 console.log(getRating(watchList));
+
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+
+// QUestion
+{
+  /**
+    Now that you have worked through a few challenges using higher-order functions like map(), 
+    filter(), and reduce(), you now get to apply them to solve a more complex challenge.
+
+    Complete the code for the squareList function using any combination of map(), filter(), and reduce(). 
+    The function should return a new array containing the squares of only the positive integers (decimal numbers are not integers) 
+    when an array of real numbers is passed to it. 
+    An example of an array of real numbers is [-3, 4.8, 5, 3, -3.2]
+*/
+}
+
+const squareList = (arr) => {
+  // Only change code below this line
+  let newArray = arr.filter(
+    (number) => number > 0 && number % parseInt(number) === 0
+  );
+  console.log(newArray);
+
+  let returnArray = newArray.map((number) => Math.pow(number, 2));
+  return returnArray;
+  // Only change code above this line
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
