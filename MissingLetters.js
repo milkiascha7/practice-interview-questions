7e46e8775b9fa328c7ff955d99b8ed8a26f30d0a
milkiascha7 <mikehailu504@gmail.com>
2023-12-29 15:09:37 +0300

    "missing letters'

---
+++ MissingLetters.js
@@ -0,0 +1,26 @@
+{
+  /**
+    Missing letters
+    Find the missing letter in the passed letter range and return it.
+
+    If all letters are present in the range, return undefined.
+*/
+}
+function fearNotLetter(str) {
+  let currCharCode = str.charCodeAt(0);
+
+  console.log(currCharCode);
+  let missing = undefined;
+
+  str.split('').forEach((letter) => {
+    if (letter.charCodeAt(0) === currCharCode) {
+      currCharCode++;
+    } else {
+      missing = String.fromCharCode(currCharCode);
+    }
+  });
+
+  return missing;
+}
+
+console.log(fearNotLetter('abce'));
